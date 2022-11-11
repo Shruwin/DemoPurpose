7c20d5590d24028f5c11810ea77e875f7425c1b8
ASHWIN <ASHWIN@DESKTOP-VFBKQ2T>
2022-11-11 15:59:38 +0530

    Script is pushed

---
+++ TestNGProject/src/Pom/Test_script.java
@@ -0,0 +1,22 @@
+package Pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test_script
+{
+	@Test
+	public void script() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Pomclass pmc = new Pomclass(driver);
+		pmc.username("Hiiiii");
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+		Thread.sleep(2000);
+		pmc.username("Helloooo");
+	}
+}
